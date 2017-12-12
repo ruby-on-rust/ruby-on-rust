23a0071640891fc7046a7ff2cf5b06ecf97081aa
Nami.WANG <namiheike@gmail.com>
2017-12-12 23:10:13 +0800

    testing plex

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,255 @@
-use std::env;
-use std::io;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::fs::File;
+#![feature(plugin)]
+#![plugin(plex)]
 
-mod parser;
-mod interpreter;
-use interpreter::interpreter::Interpreter; // TODO got to be kidding...
+use std::io::Read;
 
-fn main() {
-    let mut interpreter = Interpreter::new();
+mod lexer {
+    #[derive(Debug, Clone)]
+    pub enum Token {
+        Ident(String),
 
-    // TODO proper cli
-    if let Some(file_path) = env::args().nth(1) {
-        let file = File::open(file_path).expect("Opening file");
+        Print,
 
-        // TODO REVISIT implement comment via parser
-        // https://github.com/nikomatsakis/lalrpop/issues/10
+        Integer(i64),
+        Equals,
+        Plus,
+        Minus,
+        Star,
+        Slash,
+        LParen,
+        RParen,
+        Semi,
 
-        // APPROACH 1
-        // let mut content = String::new();
-        // let _ = file.read_to_string(&mut content);
+        Whitespace,
+        Comment,
+    }
 
-        // APPROACH 2
-        let buf = BufReader::new(file);
-        let content: String = buf.lines()
-            .map(|l| l.expect("Parsing line"))
-            .filter(|l| !l.starts_with('#'))
-            .map(move |mut l| { l.push('\n'); l })
-            .collect();
+    lexer! {
+        fn next_token(text: 'a) -> (Token, &'a str);
 
-        println!("{:?}", content);
+        r#"[ \t\r\n]+"# => (Token::Whitespace, text),
+        // "C-style" comments (/* .. */) - can't contain "*/"
+        r#"/[*](~(.*[*]/.*))[*]/"# => (Token::Comment, text),
+        // "C++-style" comments (// ...)
+        r#"//[^\n]*"# => (Token::Comment, text),
 
-        let stmts_ast = parser::parser::parse_Stmts(&content).expect("Parsing file");
+        r#"print"# => (Token::Print, text),
 
-        interpreter.eval_stmts(stmts_ast);
-    } else {
-        let mut input_line = String::new();
-        loop {
-            print!("> ");
-            let _ = io::stdout().flush();
+        r#"[0-9]+"# => {
+            (if let Ok(i) = text.parse() {
+                Token::Integer(i)
+            } else {
+                panic!("integer {} is out of range", text)
+            }, text)
+        },
 
-            io::stdin().read_line(&mut input_line).ok().expect("input failed");
+        r#"[a-zA-Z_][a-zA-Z0-9_]*"# => (Token::Ident(text.to_owned()), text),
 
-            match input_line.trim() {
-                "exit" => break,
-                line => {
-                    let line = line.to_string();
+        r#"="# => (Token::Equals, text),
+        r#"\+"# => (Token::Plus, text),
+        r#"-"# => (Token::Minus, text),
+        r#"\*"# => (Token::Star, text),
+        r#"/"# => (Token::Slash, text),
+        r#"\("# => (Token::LParen, text),
+        r#"\)"# => (Token::RParen, text),
+        r#";"# => (Token::Semi, text),
 
-                    let stmt_ast = parser::parser::parse_Stmt(&line).expect("Parsing line");
+        r#"."# => panic!("unexpected character: {}", text),
+    }
 
-                    interpreter.context.eval_stmt(*stmt_ast);
-                }
-            }
+    pub struct Lexer<'a> {
+        original: &'a str,
+        remaining: &'a str,
+    }
 
-            input_line.clear();
+    impl<'a> Lexer<'a> {
+        pub fn new(s: &'a str) -> Lexer<'a> {
+            Lexer { original: s, remaining: s }
         }
     }
+
+    #[derive(Debug, Clone, Copy)]
+    pub struct Span {
+        pub lo: usize,
+        pub hi: usize,
+    }
+
+    fn span_in(s: &str, t: &str) -> Span {
+        let lo = s.as_ptr() as usize - t.as_ptr() as usize;
+        Span {
+            lo: lo,
+            hi: lo + s.len(),
+        }
+    }
+
+    impl<'a> Iterator for Lexer<'a> {
+        type Item = (Token, Span);
+        fn next(&mut self) -> Option<(Token, Span)> {
+            loop {
+                let tok = if let Some(tok) = next_token(&mut self.remaining) {
+                    tok
+                } else {
+                    return None
+                };
+                match tok {
+                    (Token::Whitespace, _) | (Token::Comment, _) => {
+                        continue;
+                    }
+                    (tok, span) => {
+                        return Some((tok, span_in(span, self.original)));
+                    }
+                }
+            }
+        }
+    }
+}
+
+mod ast {
+    use lexer::Span;
+
+    #[derive(Debug)]
+    pub struct Program {
+        pub stmts: Vec<Expr>
+    }
+
+    #[derive(Debug)]
+    pub struct Expr {
+        pub span: Span,
+        pub node: Expr_,
+    }
+
+    #[derive(Debug)]
+    pub enum Expr_ {
+        Add(Box<Expr>, Box<Expr>),
+        Sub(Box<Expr>, Box<Expr>),
+        Mul(Box<Expr>, Box<Expr>),
+        Div(Box<Expr>, Box<Expr>),
+        Var(String),
+        Assign(String, Box<Expr>),
+        Print(Box<Expr>),
+        Literal(i64),
+    }
+}
+
+mod parser {
+    use ::ast::*;
+    use ::lexer::Token::*;
+    use ::lexer::*;
+    parser! {
+        fn parse_(Token, Span);
+
+        // combine two spans
+        (a, b) {
+            Span {
+                lo: a.lo,
+                hi: b.hi,
+            }
+        }
+
+        program: Program {
+            statements[s] => Program { stmts: s }
+        }
+
+        statements: Vec<Expr> {
+            => vec![],
+            statements[mut st] assign[e] Semi => {
+                st.push(e);
+                st
+            }
+        }
+
+        assign: Expr {
+            Print assign[a] => Expr {
+                span: span!(),
+                node: Expr_::Print(Box::new(a)),
+            },
+            Ident(var) Equals assign[rhs] => Expr {
+                span: span!(),
+                node: Expr_::Assign(var, Box::new(rhs)),
+            },
+            term[t] => t,
+        }
+
+        term: Expr {
+            term[lhs] Plus fact[rhs] => Expr {
+                span: span!(),
+                node: Expr_::Add(Box::new(lhs), Box::new(rhs)),
+            },
+            term[lhs] Minus fact[rhs] => Expr {
+                span: span!(),
+                node: Expr_::Sub(Box::new(lhs), Box::new(rhs)),
+            },
+            fact[x] => x
+        }
+
+        fact: Expr {
+            fact[lhs] Star atom[rhs] => Expr {
+                span: span!(),
+                node: Expr_::Mul(Box::new(lhs), Box::new(rhs)),
+            },
+            fact[lhs] Slash atom[rhs] => Expr {
+                span: span!(),
+                node: Expr_::Div(Box::new(lhs), Box::new(rhs)),
+            },
+            atom[x] => x
+        }
+
+        atom: Expr {
+            // round brackets to destructure tokens
+            Ident(i) => Expr {
+                span: span!(),
+                node: Expr_::Var(i),
+            },
+            Integer(i) => Expr {
+                span: span!(),
+                node: Expr_::Literal(i),
+            },
+            LParen assign[a] RParen => a
+        }
+    }
+
+    pub fn parse<I: Iterator<Item=(Token, Span)>>(i: I) -> Result<Program, (Option<(Token, Span)>, &'static str)> {
+        parse_(i)
+    }
+}
+
+mod interp {
+    use ::ast::*;
+    use std::collections::HashMap;
+
+    pub fn interp<'a>(p: &'a Program) {
+        let mut env = HashMap::new();
+        for expr in &p.stmts {
+            interp_expr(&mut env, expr);
+        }
+    }
+    fn interp_expr<'a>(env: &mut HashMap<&'a str, i64>, expr: &'a Expr) -> i64 {
+        use ::ast::Expr_::*;
+        match expr.node {
+            Add(ref a, ref b) => interp_expr(env, a) + interp_expr(env, b),
+            Sub(ref a, ref b) => interp_expr(env, a) - interp_expr(env, b),
+            Mul(ref a, ref b) => interp_expr(env, a) * interp_expr(env, b),
+            Div(ref a, ref b) => interp_expr(env, a) / interp_expr(env, b),
+            Assign(ref var, ref b) => {
+                let val = interp_expr(env, b);
+                env.insert(var, val);
+                val
+            }
+            Var(ref var) => *env.get(&var[..]).unwrap(),
+            Literal(lit) => lit,
+            Print(ref e) => {
+                let val = interp_expr(env, e);
+                println!("{}", val);
+                val
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let lexer = lexer::Lexer::new(&s);
+    let program = parser::parse(lexer).unwrap();
+    interp::interp(&program);
 }
