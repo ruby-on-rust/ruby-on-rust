a5816af804200dc999248d094d7102fe4413ed81
Nami.WANG <namiheike@gmail.com>
2017-12-27 21:58:37 +0800

    cleanup

--- build.rs
+++
@@ -1,8 +0,0 @@
-extern crate lalrpop;
-
-fn main() {
-    lalrpop::Configuration::new()
-        .log_verbose()
-        .process_file("./src/parser/parser.lalrpop")
-        .unwrap();
-}
--- src/interpreter/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod interpreter;
-pub mod context;
-pub mod object;
-pub mod classes;
-pub mod memory;
-pub mod scope;
-pub mod arithmetic;
