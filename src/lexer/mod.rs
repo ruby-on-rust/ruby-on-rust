e3144991f23bd70c7de19b3628c648570957000f
Nami.WANG <namiheike@gmail.com>
2018-02-05 20:44:29 +0800

    basically macro-izing action creating

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -21,7 +21,6 @@ pub struct Lexer {
 
 impl Lexer {
     pub fn new(input_string: String) -> Lexer {
-
         let patterns = matching_patterns::construct();
 
         let mut shared_actions: HashMap<&'static str, ActionProc> = HashMap::new();
@@ -33,65 +32,54 @@ impl Lexer {
             }
         );
 
-        // TODO macro-ize
         let mut transactions = HashMap::new();
+
+        macro_rules! action {
+            ($pattern_name:expr, $procedure:expr) => {
+                box Action {
+                    regex: patterns.get($pattern_name).unwrap().clone(), // TODO clone?
+                    procedure: $procedure
+                }
+            };
+        }
 
         transactions.insert(get_state!("line_begin"), vec![
 
             // original action for c_any
-            box Action {
-                regex: patterns.get("c_any").unwrap().clone(), // TODO clone?
-                procedure: |lexer: &mut Lexer| {
-                    println!("action invoked for c_any");
+            action!("c_any", |lexer: &mut Lexer| {
+                println!("action invoked for c_any");
 
-                    lexer.input_stream.simulate_fhold();
-                    lexer.state = LexingState::ExprValue;
-                }
-            },
+                lexer.input_stream.simulate_fhold();
+                lexer.state = LexingState::ExprValue;
+            }),
 
             // TODO
             // original action for eof
-            box Action {
-                regex: patterns.get("c_eof").unwrap().clone(), // TODO clone?
-                procedure: shared_actions.get("do_eof").unwrap().clone()
-            }
+            action!("c_eof", shared_actions.get("do_eof").unwrap().clone()),
         ]);
 
         transactions.insert(get_state!("expr_value"), vec![
             // original action for c_any
-            box Action {
-                regex: patterns.get("c_any").unwrap().clone(), // TODO clone?
-                procedure: |lexer: &mut Lexer| {
-                    println!("action invoked for c_any");
+            action!("c_any", |lexer: &mut Lexer| {
+                println!("action invoked for c_any");
 
-                    lexer.input_stream.simulate_fhold();
-                    lexer.state = LexingState::ExprBegin;
-                }
-            }
+                lexer.input_stream.simulate_fhold();
+                lexer.state = LexingState::ExprBegin;
+            }),
 
             // TODO
             // original action for eof
         ]);
 
         transactions.insert(get_state!("expr_begin"), vec![
-            // original action for r"[+\-] w_any* [0-9]"
-            // box Action {
-            //     regex: Regex::new(r"^[+\-][[:space:]]*[0-9]").unwrap(), //r"[+\-] w_any* [0-9]"
-            //     procedure: |lexer: &mut Lexer| {
-            //         println!("proc in action");
-            //     }
-            // }
-
             // original action for c_any
-            box Action {
-                regex: patterns.get("c_any").unwrap().clone(),
-                procedure: |lexer: &mut Lexer| {
+            action!("c_any", |lexer: &mut Lexer| {
                     println!("action invoked for c_any");
 
                     lexer.input_stream.hold_current_token();
                     lexer.state = LexingState::ExprEnd;
                 }
-            }
+            )
         ]);
 
         transactions.insert(get_state!("expr_end"), vec![
@@ -99,48 +87,37 @@ impl Lexer {
             // original action for:
             //     [1-9] digit* '_'? %{ @num_base = 10; @num_digits_s = @ts } int_dec
 
-            box Action {
-                // TODO handle error :trailing_in_number
-                regex: patterns.get("int_dec").unwrap().clone(),
-                procedure: |lexer: &mut Lexer| {
-                    println!("action invoked for int_dec");
+            action!("int_dec", |lexer: &mut Lexer| {
+                println!("action invoked for int_dec");
 
-                    println!("current {:?}", lexer.input_stream.current_matched_token().unwrap());
+                println!("current {:?}", lexer.input_stream.current_matched_token().unwrap());
 
-                    let parsed_int = lexer.input_stream.current_matched_token().unwrap().parse::<i64>().unwrap();
+                let parsed_int = lexer.input_stream.current_matched_token().unwrap().parse::<i64>().unwrap();
 
-                    lexer.emit_token(Token::T_INTEGER(parsed_int));
-                }
-            },
+                lexer.emit_token(Token::T_INTEGER(parsed_int));
+            }),
 
             // TODO NOT CORRESPONDING
             // original action for:
             //     w_newline
 
-            box Action {
-                regex: patterns.get("c_eol").unwrap().clone(),
-                procedure: |lexer: &mut Lexer| {
-                    println!("action invoked for c_eol");
+            action!("c_eol", |lexer: &mut Lexer| {
+                println!("action invoked for c_eol");
 
-                    // TODO
-                    // any
-                    // => { emit(:tNL, nil, @newline_s, @newline_s + 1)
-                    //     fhold; fnext line_begin; fbreak; };
+                // TODO
+                // any
+                // => { emit(:tNL, nil, @newline_s, @newline_s + 1)
+                //     fhold; fnext line_begin; fbreak; };
 
-                    lexer.input_stream.simulate_fhold();
-                    lexer.state = LexingState::LineBegin;
-                    lexer.flag_breaking();
-                }
-            },
+                lexer.input_stream.simulate_fhold();
+                lexer.state = LexingState::LineBegin;
+                lexer.flag_breaking();
+            }),
 
             // original action for:
             //     c_eof
 
-            box Action {
-                // TODO handle error :trailing_in_number
-                regex: patterns.get("c_eof").unwrap().clone(),
-                procedure: shared_actions.get("do_eof").unwrap().clone() // TODO clone?
-            }
+            action!("c_eof", shared_actions.get("do_eof").unwrap().clone())
 
         ]);
 
