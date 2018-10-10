18b770979c8ce710a284283c4f1d8c08e03f6a82
Nami WANG <namiheike@gmail.com>
2018-10-10 16:12:34 +0800

    CI (#1)
    * WIP on conf CI for build script
    
    * WIP on CI and building script
    
    * CI WIP on yarn
    
    * CI WIP conf building script

--- src/lexer/generator/lexer_generator.rb
+++ src/lexer/generator/lexer_generator.rb
@@ -1,6 +1,6 @@
 # based on https://github.com/whitequark/parser/blob/b3a0cd6be2f2d498c36cd5ae0dd39d9d25497c53/lib/parser/lexer.rl
 
-require 'byebug'
+# require 'byebug'
 
 $actions = {}
 $machines = {}
