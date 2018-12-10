e26dc353cede4262ddaed19a54a6083df42c2f6e
Nami.W <namiheike@gmail.com>
2018-12-10 22:01:38 +0800

    WIP defined nested class

--- src/interpreter/class.rs
+++ src/interpreter/class.rs
@@ -13,12 +13,11 @@ use crate::{
 impl ObjectSpace {
     pub fn predefine_classes(&mut self) {
         // Module
-        let module_id = self.def_class(String::from("Module")).unwrap();
+        let module = self.def_class(String::from("Module")).unwrap();
 
         // Class
-        // Class is under Module, but is as well in constants of Object
-        // TODO nested under module
-        // self.def_class(String::from("Class"), module_id);
+        // Class is under Module, still, it's in constants of Object.
+        self.def_nested_class(String::from("Class"), module);
     }
 
     // 
@@ -26,23 +25,34 @@ impl ObjectSpace {
     // 
     // A top-level class is a class whose superclass is Object, with the exception of BasicObject.
     // 
-    pub fn def_class(&mut self, name: String) -> Option<ObjectId> { // TODO use a Result
+    // TODO
+    // use a Result,
+    // or, could defining a class failed?
+    pub fn def_class(&mut self, name: String) -> Option<ObjectId> {
         let class_obj_value = Value::Class( ClassValue::new( self.root_object_id ) );
         let class_obj_id = self.add(class_obj_value);
 
-        self.root_obj_consts_add(name, class_obj_id);
+        self.add_const_in_superclass(self.root_object_id, name, class_obj_id);
 
         Some(class_obj_id)
     }
 
-    pub fn def_nested_class(&mut self, name: String) {
-        // superclass: ObjectId
+    // TODO
+    // use a Result,
+    // or, could defining a class failed?
+    pub fn def_nested_class(&mut self, name: String, superclass: ObjectId) -> Option<ObjectId> {
         // TODO assert superclass is actually a Class
-        panic!();
+
+        let class_obj_value = Value::Class( ClassValue::new( superclass ) );
+        let class_obj_id = self.add(class_obj_value);
+
+        self.add_const_in_superclass(superclass, name, class_obj_id);
+
+        Some(class_obj_id)
     }
 
-    fn root_obj_consts_add(&mut self, name: String, r#const: ObjectId) {
-        if let Value::Class(ref mut class) = (*self.get_root_obj()).value {
+    fn add_const_in_superclass(&mut self, superclass: ObjectId, name: String, r#const: ObjectId) {
+        if let Value::Class(ref mut class) = (*self.get(superclass)).value {
             class.consts_add(name, r#const);
         } else { unreachable!(); }
     }
