cdf6103a64558de2c2263638dcaa85d99fb66a09
Nami.W <namiheike@gmail.com>
2019-01-09 17:15:10 +0800

    refined parser monitor-builder

--- scripts/monitor.rb
+++ scripts/monitor.rb
@@ -2,15 +2,15 @@
 
 require 'bundler/inline'
 require_relative 'transform_lexer.rb'
+require_relative 'build_parser.rb'
 
 gemfile do
   source 'https://rubygems.org'
   gem 'listen', '~> 3.0'
-  gem 'byebug'
 end
 
 # lexer
-listener = Listen.to './src/lexer', only: /\.rs\.rl$/, ignore: [/tmp/], latency: 5 do |modified, added, removed|
+lexer_listener = Listen.to './src/lexer', only: /\.rs\.rl$/, ignore: [/tmp/], latency: 1 do |modified, added, removed|
   puts "modified absolute path: #{modified}"
   puts "added absolute path: #{added}"
   puts "removed absolute path: #{removed}"
@@ -19,7 +19,7 @@ listener = Listen.to './src/lexer', only: /\.rs\.rl$/, ignore: [/tmp/], latency:
 
   # transform syntax sugars
   puts 'transforming lexer...'
-  transform!
+  transform_lexer!
 
   # build lexer via ragel
   puts `../ragel-dest/bin/ragel-rust ./src/lexer/tmp/lexer.rs.rl -o ./src/lexer/lexer.rs`
@@ -31,5 +31,15 @@ listener = Listen.to './src/lexer', only: /\.rs\.rl$/, ignore: [/tmp/], latency:
   puts 'lexer rebuilt'
 end
 
-listener.start
+# parser
+parser_listener = Listen.to './src/parser', only: /\.g$/, latency: 1 do |modified, added, removed|
+  puts 'rebuilding parser...'
+
+  build_parser!
+
+  puts 'parser rebuilt'
+end
+
+lexer_listener.start
+parser_listener.start
 sleep
